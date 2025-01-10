5f6524e976957fec10278b8d8d15b3357ab22afc
Omolemo Tlomatsane <tycotecsa@gmail.com>
2025-01-10 15:00:25 +0200

    Added a dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // You can leave out root if you use the default structure
-  root: './public', // if necessary, to ensure the app is built from the correct directory
-})
+});
 
