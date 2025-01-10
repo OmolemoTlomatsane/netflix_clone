ef75b969bb3a30102dab8eb3cee0b1e7222408e7
Omolemo Tlomatsane <tycotecsa@gmail.com>
2025-01-10 13:12:12 +0200

    Build project for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // You can leave out root if you use the default structure
+  root: './public', // if necessary, to ensure the app is built from the correct directory
 })
+
