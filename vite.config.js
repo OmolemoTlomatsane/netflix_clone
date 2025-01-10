b0b90311b2872a39367d0d465118dffb60a6ed7c
Omolemo Tlomatsane <tycotecsa@gmail.com>
2025-01-10 16:33:40 +0200

    added the dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Set the root directory to the project root
+  build: {
+    outDir: 'dist', // Directory to output built files
+    emptyOutDir: true, // Clean the output directory before building
+  },
 });
 
